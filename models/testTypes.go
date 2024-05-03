99b21cdf56dd25868283439d3a4957734cc2ed0e
Jane Li <jane.li@uber.com>
2024-05-03 13:49:12 -0700

    add test types

---
+++ models/testTypes.go
@@ -0,0 +1,11 @@
+package model
+
+type User struct {
+		ID string `json:"id"`
+		FirstName string `json:"firstName"`
+		LastName string `json:"lastName"`
+		PictureURL string `json:"pictureURL"`
+		PhoneNumber string `json:"phoneNumber"`
+		Email float64
+		Rating *Rating `json:"rating"`
+}
